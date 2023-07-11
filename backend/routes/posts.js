40a5e0b9b785d49211f7ada1d029b8ea8b99879d
Siarhei Pats <pats.siarhei@gmail.com>
2023-07-11 17:56:06 -0400

    App Setup
    Added routes folder and set the layout for future routes to perform CRUD with user posts.

---
+++ backend/routes/posts.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const router = express.Router();
+
+// Get all posts
+router.get('/', (req, res) => {
+    res.json({ mssg: "GET all posts" })
+});
+
+// Get one post by ID
+router.get('/:id', (req, res) => {
+    res.json({ mssg: `GET a post by ID` })
+});
+
+// Add a post
+router.post('/', (req, res) => {
+    res.json({ mssg: "POST a post" })
+});
+
+// Delete a post by ID
+router.delete('/:id', (req, res) => {
+    res.json({ mssg: "DELETE a post by ID" })
+});
+
+// Update a post by ID
+router.patch('/:id', (req, res) => {
+    res.json({ mssg: "PATCH a post by ID" })
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ require('dotenv').config()
 
 const express = require("express");
 
+// routers
+const postRoutes = require("./routes/posts");
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ mssg: "Software Modernization Backend" })
 })
+
+app.use('/posts', postRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on ${process.env.PORT}`)
