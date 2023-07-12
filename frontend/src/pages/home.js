6c279d5a4d187e501b14aae752a4106e31dcb6e0
Siarhei Pats <pats.siarhei@gmail.com>
2023-07-11 20:58:42 -0400

    Landing Page
    Added landing page to the front end

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,8 +1,21 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
 const Home = () => {
     return (
-        <div>
-            <h2>Front End App</h2>
-        </div>
+        <Box sx={{ display: 'flex' }}>
+            <List component="nav" aria-label="mailbox folders">
+                <ListItem>
+                    <ListItemText primary="Software Modernization" />
+                </ListItem>
+                <ListItem>
+                    <CircularProgress />
+                </ListItem>
+            </List>
+        </Box>
     )
 }
 
